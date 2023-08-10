3c6069161a96f03b574c671df9278f2a42c4426e
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-10 16:31:13 +0300

    use ref

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,66 +1,60 @@
 import React from 'react';
 
 class Form extends React.Component {
-  state = {
-    email: '',
-    isAgreeWithTerms: false,
-  };
+  constructor() {
+    super();
+    this.state = {
+      card: '',
+      email: '',
+    };
+    this.firstNameRef = React.createRef();
+    this.emailRef = React.createRef();
+  }
 
   handleEmail = (event) => {
-    this.setState({ [event.target.name]: event.target.value }); // создаем динамич. ключ
+    this.setState({ email: event.target.value }); // создаем динамич. ключ
   };
 
-  handleCheckboxChange = (event) => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
-
-  submitForm = () => {
-    const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-      this.state.email
+  handleChange = (event) => {
+    this.setState(
+      () => ({ [event.target.name]: event.target.value }),
+      () => {
+        if (this.state.card.length === 16) {
+          // после того как мы вбили 16 символов
+          // то установим фокус на след. инпуте
+          this.emailRef.current.focus();
+        }
+      }
     );
-    const isValidCheckbox = this.state.isAgreeWithTerms;
-
-    if (!isValidEmail) {
-      alert('Your email is not valid');
-      return;
-    }
-
-    if (!isValidCheckbox) {
-      alert('Your should accept all terms and conditions');
-      return;
-    }
-    this.setState({
-      email: '',
-      isAgreeWithTerms: false,
-    });
-    alert('Thank you for subscription!');
   };
+
+  componentDidMount() {
+    // console.log(this.firstNameRef);
+    this.firstNameRef.current.focus();
+  }
 
   render() {
-    const { email, isAgreeWithTerms } = this.state;
+    const { email, card } = this.state;
     return (
       <div>
+        <input
+          type="number"
+          name="card"
+          value={card}
+          placeholder="card"
+          onChange={this.handleChange}
+          ref={this.firstNameRef}
+        />
+        <br />
         <input
           type="email"
           name="email"
           placeholder="email"
           value={email}
           onChange={this.handleEmail}
+          ref={this.emailRef}
         />
         <br />
-        <label>
-          <input
-            type="checkbox"
-            name="isAgreeWithTerms"
-            checked={isAgreeWithTerms}
-            onChange={this.handleCheckboxChange}
-          />
-          Subscription
-        </label>
-        <br />
-        <button type="button" onClick={this.submitForm}>
-          Send
-        </button>
       </div>
     );
   }
